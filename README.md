ab7561bb1591d2e31a4afab86926cbc7d072cf29
eineflocke <usk45a@gmail.com>
2022-05-03 02:17:01 +0900

    20220502

---
+++ README.md
@@ -0,0 +1,41 @@
+# advanced-select-multiple
+
+more useful `<select multiple>`
+just click (no Ctrl needed) and select/unselect from list
+
+## usage
+
+1. load `asm.js`
+2. prepare a DOM element containing vacant `<select class="asm-noselect" multiple>` and `<select class="asm-selected" multiple>`
+
+```html
+<div id="test">
+  <select class="asm-noselect" multiple></select>
+  <select class="asm-selected" multiple></select>
+</div>
+```
+
+3. add styling
+4. make a new instance from `Asm` with object/array for option initialization
+
+```js
+const asm = new Asm('#test', {
+  option1: 'option with value "option1"',
+  option2: 'more option with value "option2"',
+  option3: 'another option with value "option3"'
+});
+
+new Asm('.test2', [1, 2, 3, 5, 8, 13, 21]);
+
+new Asm('.test3', {
+  a: {
+    text: 'pre-selected option',
+    selected: true
+  },
+  b: {
+    text: 'non-pre-selected option',
+    selected: false
+  },
+  c: 'default is non-pre-selected'
+});
+```
---
+++ asm.js
@@ -0,0 +1,84 @@
+'use strict';
+
+class Asm {
+  constructor(sel, obj) {
+    this.errorprefix = 'asm: ';
+
+    const q = document.querySelector(sel);
+    if (!q) throw `${this.errorprefix}css-selector ${sel} not found`;
+
+    this.q = q;
+
+    if (Array.isArray(obj)) {
+      const obj2 = {};
+      for (const o of obj) {
+        const ostr = String(o);
+        obj2[ostr] = ostr;
+      }
+      obj = obj2;
+    } else if (typeof obj !== 'object') {
+      throw `${this.errorprefix}obj is neither array nor object`;
+    }
+
+    for (const sel2 of ['.asm-selected', '.asm-noselect']) {
+
+      const q2 = q.querySelector(sel2);
+      if (!q2) throw `${this.errorprefix}css-selector ${sel2} not found`;
+
+      for (const key of Object.keys(obj)) {
+        const opt = document.createElement('option');
+        opt.value = key;
+        if (typeof obj[key] === 'string') {
+          opt.text = obj[key];
+        } else if (obj[key].text) {
+          opt.text = obj[key].text;
+        }
+        if (typeof obj[key].selected === 'boolean') {
+          opt.disabled = obj[key].selected !== (sel2 === '.asm-selected');
+        } else {
+          if (sel2 === '.asm-selected') opt.disabled = 'disabled';
+        }
+        q2.appendChild(opt);
+      }
+
+      const counthandler = () => {
+        for (const sel of ['.asm-selected', '.asm-noselect']) {
+          const qnum = q.querySelector(`${sel}-num`);
+          if (!qnum) continue;
+          qnum.innerHTML = Array.from(q.querySelector(sel).options).filter(o => !o.disabled).length;
+        }
+      };
+
+      const clickhandler = (e, sel2) => {
+        if (e.target.tagName !== 'OPTION') return false;
+        e.preventDefault();
+
+        const sel3 = sel2 === '.asm-selected' ? '.asm-noselect' : '.asm-selected';
+        const scrolltop = document.querySelector(sel3).scrollTop;
+        q.querySelector(`${sel3} option[value="${e.target.value}"]`).disabled = null;
+        e.target.disabled = 'disabled';
+        q.querySelectorAll(`.asm-selected option`).forEach(qq => qq.selected = 'selected');
+        setTimeout(() => document.querySelector(sel2).scrollTop = scrolltop, 0);
+
+        counthandler();
+      };
+
+      q2.addEventListener('click', e => clickhandler(e, sel2));
+      q2.addEventListener('mousemove', e => e.preventDefault());
+
+      counthandler();
+
+    } // for sel2
+
+    document.write(`<style>${sel} option:disabled { display: none; }</style>`);
+  } // constructor
+
+  getselected(attr = 'value') {
+    if (attr !== 'value' && attr !== 'text') throw `${this.errorprefix}invalid attribute`;
+    const ret = [];
+    this.q.querySelectorAll('.asm-selected option:not([disabled])').forEach(q => {
+      ret.push(q[attr]);
+    });
+    return ret;
+  }
+}
